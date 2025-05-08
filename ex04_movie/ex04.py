1a5e39347610e7505538a6dfb574f0d437a920a1
Beyyy <148631148+iobey77ok@users.noreply.github.com>
2025-05-08 10:05:35 +0000

    renaming fields, filter out movies with no rating

---
+++ ex04_movie/ex04.py
@@ -0,0 +1,36 @@
+#%%
+import pandas as pd
+df = pd.read_csv('movies.csv')
+df
+#%%
+df.columns = df.columns.str.lower()
+df
+#%%
+df.rename(columns={
+    'one-line' : 'brief summary',
+    'stars' : 'actors'
+}, inplace=True)
+df
+#%% replacing \n
+df['genre'] = df['genre'].str.replace(r'[\n]', '', regex=True)
+df
+
+#%% apply replacing to all columns selected
+# when selecting many cols need to be in dataframe
+df[['genre', 'brief summary', 'actors']] = df[['genre', 'brief summary', 'actors']].apply(
+    lambda x : x.str.replace(r'[\n]', '', regex=True)
+)
+df
+
+#%%
+df['brief summary'] = df['brief summary'].replace('Add a Plot', 'Plot not Available')
+df
+# %%
+mask1 = df.eval('rating.isnull()')
+mask1
+# %%
+df[~mask1]
+# %%
+# extract director and actors
+
+# make it into separate columns
