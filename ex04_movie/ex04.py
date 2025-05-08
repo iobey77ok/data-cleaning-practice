e8513c24be71b5d8bb3e558feda6588fd8f036cd
Beyyy <148631148+iobey77ok@users.noreply.github.com>
2025-05-08 12:24:43 +0000

    add split director and actor code

--- ex04_movie/ex04.py
+++ ex04_movie/ex04.py
@@ -30,7 +30,22 @@ mask1 = df.eval('rating.isnull()')
 mask1
 # %%
 df[~mask1]
+
+#%% delete runtime
+df = df.drop(columns=['runtime','gross'], axis=1)
+df
 # %%
 # extract director and actors
-
 # make it into separate columns
+
+df['director'] = df['actors'].str.extract(r'Director:\s*([\w\s,]+)', expand=False)
+df['actor'] = df['actors'].str.extract(r'Stars?:\s*([\w\s,]+)', expand=False)
+df = df.drop('actors', axis = 1)
+df
+# %%
+
+mask2 = df['rating'].isnull() | df['director'].isnull() | df['actor'].isnull()
+df[~mask2]
+# %%
+df.dtypes
+# %%
